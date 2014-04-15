01b4fe9cc23e54e9a4cd4072d553b50128ff5b01
dan.graham <dpgraham.85@gmail.com>
2014-04-14 21:09:12 -0700

    Added unit test to confirm that the package works correctly with RequireJS

--- test/slacker-window.test.js
+++ test/slacker-window.test.js
@@ -1,12 +1,24 @@
 suite('invoke various methods and test the results', function(){
-    var slacker = new Slacker("../dist/slacker-worker.js");
+    test('confirm that it is working the requirejs', function(done){
+        require(["/dist/slacker-window.js"], function(Slacker){
+
+            // Just confirm that we required it correctly and check that 'invoke' (one of it's method) exists
+            // to make sure it's the right module
+            chai.assert.ok(!!Slacker && !!Slacker.prototype.invoke);
+            window.Slacker = Slacker;
+            done();
+        });
+    });
+
     test('invoke an undefined method', function(done){
+        var slacker = new Slacker("../dist/slacker-worker.js");
         slacker.invoke("foo", undefined, undefined, function(res){
             done();
         });
     });
 
     test('test the deflate method', function(done){
+        var slacker = new Slacker("../dist/slacker-worker.js");
         slacker.invoke("deflate", "FLIPPITY FLAPPITY", function(res){
             chai.assert.ok(res.length < "FLIPPITY FLAPPITY".length)
             slacker.invoke("inflate", res, function(decompressed){
